99db65fe3bcdaeb472fe41d59ac9852009727d34
lingyv <lingyv@gmail.com>
2017-09-30 17:13:55 +0800

    完成事件记录

--- controller.js
+++ controller.js
@@ -2,8 +2,8 @@
 
 var Koa = require('koa');
 var app = new Koa();
-
 const router = require('koa-router')();
+var event = require('./service/event');
 
 // time
 app.use(function *(next){
@@ -21,13 +21,17 @@ app.use(function *(next){
   console.log('%s %s - %s', this.method, this.url, ms);
 });
 
+//记录事件接口,请求格式 --> http://localhost:3000/event?e=阅读《SICP》&d=2hr
 router.get('/event', async (ctx, next) => {
-    var event = ctx.query.e || '';
-    var duration = ctx.query.d || '';
-    ctx.response.body = `<h1>Hello, ${event} --> ${duration}!</h1>`;
-
+    var e = ctx.query.e || '';
+    var d = ctx.query.d || '';
+    ctx.response.body = `<h1>Hello, ${e} --> ${d}!</h1>`;
+    if (e !== '' && d !== '') {
+        event.saveEvent({event : e, duration: d});
+    }
 });
 
+//根路径应答
 router.get('/', async (ctx, next) => {
     ctx.response.body = '<h1>Hello World!</h1>';
 });
